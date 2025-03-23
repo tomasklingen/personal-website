26ab5f619766e61471c04d728f7db56c12bf0401
Your Name <you@example.com>
2025-03-23 10:44:37 +0100

    chore: include run in vite config so it is included in build dist output

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,13 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+	build: {
+		rollupOptions: {
+			input: {
+				main: 'index.html',
+				run: 'run/index.html',
+			},
+		},
+	},
 	plugins: [tailwindcss()],
 })

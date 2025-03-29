13bb84c2cf18d5fb111a7f974d5d77e98919b219
Your Name <you@example.com>
2025-03-29 18:14:16 +0100

    👨‍💻 favicon

--- app/root.tsx
+++ app/root.tsx
@@ -14,6 +14,11 @@ export const links: Route.LinksFunction = () => [
 		rel: 'stylesheet',
 		href: 'https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&display=swap',
 	},
+	{
+		rel: 'icon',
+		href: '/favicon.ico',
+		sizes: '48x48',
+	},
 ]
 
 export function Layout({ children }: { children: React.ReactNode }) {

bf3babe400277f4568ed6c70bcf60ca6ef0e2204
Tomas Klingen <tomasklingen@users.noreply.github.com>
2025-08-02 23:18:10 +0200

    feat: setup development tooling with prettier and workspace config
    - Update .gitignore to exclude entire .vscode/ directory
    - Add pnpm workspace configuration
    - Add prettier config with Astro plugin support
    - Disable Biome formatter in favor of Prettier
    - Configure import organization settings in Biome

---
+++ prettier.config.js
@@ -0,0 +1,25 @@
+
+/**
+ * @type {import('prettier').Config}
+ * @see https://prettier.io/docs/en/configuration.html
+ */
+const config = {
+    "plugins": [
+        "prettier-plugin-astro"
+    ],
+    "overrides": [
+        {
+            "files": "*.astro",
+            "options": {
+                "parser": "astro"
+            }
+        }
+    ],
+    "printWidth": 80,
+    "tabWidth": 2,
+    "useTabs": true,
+    "semi": false,
+    "singleQuote": true
+}
+
+export default config;

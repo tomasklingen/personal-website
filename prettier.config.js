5685f1898416b6960d29ac8ab6d237a08a9b4c53
Tomas Klingen <tomasklingen@users.noreply.github.com>
2025-08-12 16:12:58 +0200

    style: update Prettier config formatting

--- prettier.config.js
+++ prettier.config.js
@@ -1,25 +1,28 @@
-
 /**
  * @type {import('prettier').Config}
  * @see https://prettier.io/docs/en/configuration.html
  */
 const config = {
-    "plugins": [
-        "prettier-plugin-astro"
-    ],
-    "overrides": [
-        {
-            "files": "*.astro",
-            "options": {
-                "parser": "astro"
-            }
-        }
-    ],
-    "printWidth": 80,
-    "tabWidth": 2,
-    "useTabs": true,
-    "semi": false,
-    "singleQuote": true
+	plugins: ['prettier-plugin-astro'],
+	overrides: [
+		{
+			files: '*.astro',
+			options: {
+				parser: 'astro',
+			},
+		},
+		{
+			files: ['**/*.jsonc'],
+			options: {
+				trailingComma: 'none',
+			},
+		},
+	],
+	printWidth: 80,
+	tabWidth: 2,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
 }
 
-export default config;
+export default config
